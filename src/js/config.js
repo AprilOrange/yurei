046c8eb288576098d5d565f64509d31f36792257
April Orange <aprilorange.net@icloud.com>
2015-06-14 00:23:48 +0800

    update token

--- src/js/config.js
+++ src/js/config.js
@@ -3,7 +3,7 @@ var site = {
   repo: 'yurei',
   per_page: 20,
   show_excerpt: true,
-  access_token: '8b96b917bfaace41da731f4e2dcb4531b833ad01' // generated at https://github.com/settings/tokens
+  access_token: '112a520194092c174027b731a09225fa94513f3c' // yurei.avosapps.com/auth
 };
 
 var api = function(path) {
